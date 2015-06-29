357543e99df0ef5cd392c86202f563359ac06df4
ablomberg1 <ablomberg9830@att.net>
2015-06-28 23:21:57 -0700

    Updating Final
    Updated Final Assigment

--- templates.py
+++ templates.py
@@ -2,15 +2,17 @@ import os
 import jinja2
 import webapp2
 
-
-from validation import *
+from google.appengine.ext import ndb
 
 #to initialize jinja
 #directory that my current file is in os.path.dirname(__file__)
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader (template_dir),
-	autoescape = True)
+  autoescape = True)
 
+class Message(ndb.Model):
+  comment = ndb.StringProperty()
+  date = ndb.DateTimeProperty(auto_now_add=True)
 
 class Handler(webapp2.RequestHandler):
   def write(self, *a, **kw):
@@ -23,27 +25,33 @@ class Handler(webapp2.RequestHandler):
   def render(self, template, **kw):
     self.write(self.render_str(template, **kw))
 
-class MainPage(Handler):
-  def write_form(self, error="", month="", day="", year=""):
-    #dictionary that we pass into the form
-    self.response.out.write(form % {"error": error,  
-      "month": month, "day": day, "year": year})
 
+
+class MainPage(Handler):
   def get(self):
-    user_month = self.request.get('month')
-    user_day = self.request.get('day')
-    user_year = self.request.get('year')
-    self.render("form.html", user_month=user_month, user_day=user_day, user_year=user_year)
+    # error = self.request.get('error','') 
+     
+    query = Message.query().order(Message.date)
+    message_list = query.fetch()
+
+    self.render("form.html", message_list=message_list)
+    message = message_list
+    # self.response.out.write(message)
 
   def post(self):
-    month = valid_month(user_month)
-    day = valid_day(user_day)
-    year = valid_year(user_year)
-  
-    if not (month and day and year):
-      self.write_form("That doesn't look valid to me, friend.",
-            user_month, user_day, user_year)
-    else:
-      self.response.out.write("Thanks! That's a totally valid day!")
+    # date = self.request.get('date')
+    comment = self.request.get('comment')
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+    # if either of the fields (link or comment) is blank
+    if comment:
+        message = Message(comment=comment)
+        message.put()
+        import time
+        time.sleep(.1)
+        self.redirect('/')
+        self.response.out.write(message)
+    else:
+      self.response.out.write('Please leave a comment!') 
+
+app = webapp2.WSGIApplication([('/', MainPage),
+], debug=True)
